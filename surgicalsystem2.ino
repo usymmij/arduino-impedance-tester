e3c348f310821003d80b6efbfc4b7317e030d574
(∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. * ･ ｡ﾟ <36722504+usymmij@users.noreply.github.com>
2019-07-02 00:57:14 -0700

    optimized
    drastically decreased latency, and removed redundant code left over from system1
    target has also changed from 200 to 255, as it is a small change relative to 1024, and is easier to use with bitwise operators (since 1111111 is 255)

--- surgicalsystem2.ino
+++ surgicalsystem2.ino
@@ -1,111 +1,40 @@
 #define FREQUENCY 2000
 #define DURATION 1000
-#define speakerPort 11
+#define SPEAKER_PORT 11
 
-String old = "";
-int mode = 2;
 int analogPin = A0;
-int raw = 0;
-int Vin = 5;
-float Vout = 0;
-float R1 = 4700;
-float R2 = 0;
-float buffer = 0;
 
-//variables from processing
-float globalTotal = 0;//original proccessing code had bad naming practice
-float target = 100.0;
-int samples=0;
-float filterdat[3];
+float impedance = 0;
+float filterBuffer[3];
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(speakerPort, OUTPUT);
-  for (int i = 14; i < 53; i++)
-  {
-    pinMode(i, INPUT_PULLUP);
-  }
+  //Serial debug
+  //Serial.begin(9600);
 
+  //output pins
+  pinMode(13, OUTPUT);
+  pinMode(SPEAKER_PORT, OUTPUT);
+
+  //input pins and refrence pins for impedance
   pinMode(A3, OUTPUT);
-  digitalWrite(A3, LOW);
-  pinMode(A0, INPUT);
   pinMode(A2, OUTPUT);
-  digitalWrite(A2, HIGH);
+  pinMode(A0, INPUT);
   pinMode(A4, INPUT);
+
+  //set refrence pins
+  digitalWrite(A3, LOW);
+  digitalWrite(A2, HIGH);
 }
 
 void loop()
 {
-
-  if (Serial.available() > 0)
-  {
-    mode = 1;
+  //while the impedance signal is >255, set the speaker and led to on
+  while ((analogRead(analogPin) >> 7) ^ 0) {//the target is 2^8 - 1, or 255
+   tone(SPEAKER_PORT, FREQUENCY);
+    PORTB |= B00100000;
   }
-
-  if (mode == 1)
-  {
-    String output = "";
-    for (int i = 14; i < 53; i++)
-    {
-      if (digitalRead(i) == LOW)
-      { //input has been pulled down
-        output = output + (i - 13) + " ";
-      }
-    }
-  }
-  else if (mode == 2)
-  {
-    raw = analogRead(analogPin);
-
-    buffer = raw * 5;
-    Vout = (buffer) / 1024.0;
-    buffer = (Vin / Vout) - 1;
-    R2 = R1 * buffer;
-    int returned = analogRead(A3);
-    //Serial.print(R2, DEC);
-    //Serial.print(",");
-    //Serial.print(raw, DEC);
-    //Serial.print(",");
-    //Serial.print(returned, DEC);
-    //Serial.println("v");
-  }
-  //inject processing code
-  float valueOne = maFilter((float)raw);//rvalue
-  float valueTwo = (float)raw;//rvalue2
-  Serial.println(valueOne);
-  char status = '0';
-  if (samples < 10) {
-    globalTotal = globalTotal + valueOne;
-    samples++;
-    status = '1';
-  }  else
-  {
-    target = (globalTotal / samples) + 200;
-    status = '2';
-    valueOne = map(valueOne, 0, 1005, 0, 1015);
-    //the speaker draws out some of the current, so a simple software amplifier is added
-    if (valueOne >= target)
-    {
-      tone(speakerPort, FREQUENCY);
-      digitalWrite(13, HIGH);
-    } else {
-      noTone(speakerPort);
-      digitalWrite(speakerPort,LOW);
-      digitalWrite(13, LOW);
-    }
-  }
-}
-
-float maFilter(float data) {
-  double total = 0;
-  for (int i = 2; i < 3; i++)
-  {
-    filterdat[i - 1] = filterdat[i];
-    total = total + filterdat[i];
-  }
-  total = total;
-  filterdat[2] = data;
-  return (float)total;
+  //if the signal is <255, set it to off
+  noTone(SPEAKER_PORT);
+  PORTB &= B00000000;
 }
